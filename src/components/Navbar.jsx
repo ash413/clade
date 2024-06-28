61b9c8a47ca1964d6adac4c8d31843ecee430a4c
Vaishnavi Kadam <vaishnavi.kadam413@gmail.com>
2024-06-28 18:43:34 +0530

    nav-css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,28 @@
 import React from 'react'
-import {feChevronDown} from 'react-icons';
+import { FaChevronDown } from "react-icons/fa";
+import { LuBell } from "react-icons/lu";
+import { default as logo } from '../assets/logo.png';
 
 const Navbar = () => {
-  return (
-    <div>
-        <div>
-            <p>Logo</p>
+return (
+    <div className='text-black flex justify-between m-8 items-center'>
+        <div className='flex items-center bg-gray-400'>
+            <p className='text-orange'>Logo</p>
         </div>
-        <div>
+        <div className='flex justify-between border-8 m-4'>
             <button>Jobs</button>
             <p>Messages</p>
             <p>Payments</p>
         </div>
-        <div>
-            {/*Bell icon */}
-            <div>
-                <img src="" alt="" />
-                <feChevronDown />
+        <div className='flex'>
+            <LuBell className='mr-4 mt-2'/>
+            <div className='flex '>
+                <img src={logo} alt="" className='w-8 h-8 mr-2 rounded-full'/>
+                <FaChevronDown className='mt-2'/>
             </div>
         </div>
     </div>
-  )
+)
 }
 
 export default Navbar
